0d27739bc7e2a379e4ab6c7559a1dbb3b88f1c6b
Nazeh <ar.nazeh@gmail.com>
2019-10-07 13:34:25 +0300

    start render function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,12 +1,37 @@
 const myLibrary = [];
 
+
+// App Logic
+
 function Book(title, author, pages, read = false) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
 function addBookToLibrary(book) {
-  myLibrary.push(book);
+    myLibrary.push(book);
 }
+
+// populate temporarily
+
+for (let i = 0; i <= 10; i++) {
+    let book = new Book(`title ${i}`, `author ${i}`, `pages ${i}`)
+    addBookToLibrary(book)
+}
+
+// Dom Manipulation
+
+function render() {
+    for (let book of myLibrary) {
+        document.getElementById('table').innerHTML = renderBook(book)
+    }
+}
+
+function renderBook(book) {
+    return `title: ${book.title} author: ${book.author} 
+    pages: ${book.pages} read: ${book.read}`
+}
+
+render()
